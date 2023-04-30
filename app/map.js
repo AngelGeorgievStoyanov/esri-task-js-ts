c88a4c996274bbcc82f6a68b953f92fb91adcb6c
Angel <angelo_stoyanov@abv.bg>
2023-04-30 12:07:51 +0300

    add point(marker) on map

--- app/map.js
+++ app/map.js
@@ -1,12 +1,15 @@
-define(["require", "exports", "tslib", "esri/WebMap", "esri/views/MapView"], function (require, exports, tslib_1, WebMap_1, MapView_1) {
+define(["require", "exports", "tslib", "esri/Graphic", "esri/WebMap", "esri/layers/GraphicsLayer", "esri/symbols/WebStyleSymbol", "esri/views/MapView"], function (require, exports, tslib_1, Graphic_1, WebMap_1, GraphicsLayer_1, WebStyleSymbol_1, MapView_1) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     exports.mapInit = void 0;
+    Graphic_1 = tslib_1.__importDefault(Graphic_1);
     WebMap_1 = tslib_1.__importDefault(WebMap_1);
+    GraphicsLayer_1 = tslib_1.__importDefault(GraphicsLayer_1);
+    WebStyleSymbol_1 = tslib_1.__importDefault(WebStyleSymbol_1);
     MapView_1 = tslib_1.__importDefault(MapView_1);
     function mapInit(x, y, z) {
         return tslib_1.__awaiter(this, void 0, void 0, function () {
-            var webmap, view;
+            var point, webmap, view, graphicsLayer, webStyleSymbol, pointGraphic;
             return tslib_1.__generator(this, function (_a) {
                 webmap = new WebMap_1.default({
                     basemap: 'streets-vector'
@@ -17,6 +20,25 @@ define(["require", "exports", "tslib", "esri/WebMap", "esri/views/MapView"], fun
                     center: [x || 23.321590139866355, y || 42.697866831005435],
                     zoom: z
                 });
+                graphicsLayer = new GraphicsLayer_1.default();
+                webmap.add(graphicsLayer);
+                if (x && y) {
+                    point = {
+                        type: "point",
+                        longitude: x,
+                        latitude: y
+                    };
+                }
+                ;
+                webStyleSymbol = new WebStyleSymbol_1.default({
+                    name: "push-pin-1",
+                    styleName: "Esri2DPointSymbolsStyle"
+                });
+                pointGraphic = new Graphic_1.default({
+                    geometry: point,
+                    symbol: webStyleSymbol
+                });
+                graphicsLayer.add(pointGraphic);
                 view.when(function () { return console.log('view is ready!'); });
                 return [2 /*return*/];
             });
--- app/map.ts
+++ app/map.ts
@@ -1,9 +1,13 @@
+import Graphic from 'esri/Graphic';
 import WebMap from 'esri/WebMap';
+import GraphicsLayer from 'esri/layers/GraphicsLayer';
+import WebStyleSymbol from 'esri/symbols/WebStyleSymbol';
 import MapView from 'esri/views/MapView';
 
 
 export async function mapInit(x: number, y: number, z: number) {
-
+   
+    let point: {} | null;
 
 
     const webmap = new WebMap({
@@ -18,6 +22,34 @@ export async function mapInit(x: number, y: number, z: number) {
     });
 
 
+    const graphicsLayer = new GraphicsLayer();
+
+    webmap.add(graphicsLayer);
+
+
+    if (x && y) {
+
+        point = {
+            type: "point",
+            longitude: x,
+            latitude: y
+        };
+    };
+
+
+    const webStyleSymbol = new WebStyleSymbol({
+        name: "push-pin-1",
+        styleName: "Esri2DPointSymbolsStyle"
+    });
+
+
+    const pointGraphic = new Graphic({
+        geometry: point,
+        symbol: webStyleSymbol
+    });
+
+
+    graphicsLayer.add(pointGraphic);
 
 
     view.when(() => console.log('view is ready!'))
